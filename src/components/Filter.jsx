9d751752750dcbda50a455747d71341104c785bd
Wanderson <wanderson991.wb@gmail.com>
2023-07-26 00:01:13 -0300

    chore: first commit

---
+++ src/components/Filter.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+
+const Filter = ({ filter, setFilter, setSort }) => {
+  return (
+    <div className="filter">
+      <h2>Filtrar:</h2>
+      <div className="filter-options">
+        <div>
+          <p>Status:</p>
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+            <option value="All">Todos</option>
+            <option value="completed">Completas</option>
+            <option value="Incomplete">Incompletas</option>
+          </select>
+        </div>
+        <div>
+          <p>Ordem alfabética</p>
+          <button onClick={() => setSort('Asc')}>Asc</button>
+          <button onClick={() => setSort('Desc')}>Desc</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Filter
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(value !== '' || category !== '') {
+      addTodo(value, category)
+    }
+  };
+
+  return (
+    <div className="todo-form">
+      <h2>Criar tarefa:</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite o título"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="">selecione uma categoria</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Pessoal">Pessoal</option>
+          <option value="Estudos">Estudos</option>
+        </select>
+        <button type="submit">Criar tarefa</button>
+      </form>
+    </div>
+  );
+}
+
+export default TodoForm;
